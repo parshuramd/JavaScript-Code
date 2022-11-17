6b1d6bc105db85eb33ae3ca3cc28d6d212af5bb4
Parshuram Dadas <dadasparshuram96@96>
2022-11-17 12:00:04 +0530

    Started Javascript

--- break.js
+++ break.js
@@ -1,7 +1,15 @@
-for(let a=0;a<10;a++){
-    if (a==3) {
-        console.log("hey there i am number : 3");
-        break;
-    }
-    console.log(a)
+// for(let a=0;a<10;a++){
+//     if (a==3) {
+//         console.log("hey there i am number : 3");
+//         break;
+//     }
+//     console.log(a)
+// }
+
+const a = 1;
+const b = num =>{
+    num++;
+    console.log(num)
 }
+b(a)
+console.log(a)
